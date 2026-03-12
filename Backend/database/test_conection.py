ad7770811e75c8af0830204fa04d0de65fda926d
oestranhooo <matheusweiber2330@gmail.com>
2026-03-11 23:39:45 -0300

    Remove obsolete code

--- Backend/database/test_conection.py
+++
@@ -1,7 +0,0 @@
-# Importa direto do arquivo vizinho (database.py)
-from database import client
-
-response = client.table("diseases_catalog").select("*").execute()
-
-print(" Conexão realizada! Dados recebidos:")
-print(response.data)
